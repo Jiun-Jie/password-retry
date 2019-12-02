e5f15bd78006476ba30ad82f6848695de2dd14a6
JJ Chiou <jiunjiechiou@hotmail.com>
2019-12-02 23:48:25 +0800

    close the account

--- password_retry.py
+++ password_retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 x = 3
 while x > 0:
+	x = x - 1
 	password1 = input('請輸入密碼:')
 	if password == password1:
 		print('登入成功!!')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤! 還有', x,'次機會')
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x,'次機會')
+		else:
+			print('沒機會嘗試了! 要鎖帳號了啦!')
